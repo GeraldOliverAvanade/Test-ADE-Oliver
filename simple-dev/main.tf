e338bdbeb5e5f36acdecbd6bb61cd50b3e18449c
GeraldOliverAvanade <gerald.oliver.onggi@avanade.com>
2026-02-09 14:51:28 +0900

    Update main.tf

--- simple-dev/main.tf
+++ simple-dev/main.tf
@@ -1,30 +1,19 @@
-terraform {
-  required_version = ">= 1.3.0"
+@description('Environment name used as a prefix for resources')
+param envName string
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-    }
+@description('Azure region for deployment')
+param location string = resourceGroup().location
+
+// Storage account names: 3-24 chars, lowercase letters and numbers only
+var base = toLower(replace(envName, '-', ''))
+var saNameRaw = '${base}sa'
+var saName = take(saNameRaw, 24)
+
+resource storageAccount 'Microsoft.Storage/storageAccounts@2023-01-01' = {
+  name: saName
+  location: location
+  kind: 'StorageV2'
+  sku: {
+    name: 'Standard_LRS'
   }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-# If ADE is already creating/providing the resource group, you usually don't create it here.
-# You just deploy into the existing RG and location.
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = lower("${var.envName}sa")
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = var.location != "" ? var.location : data.azurerm_resource_group.rg.location
-
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
 }
