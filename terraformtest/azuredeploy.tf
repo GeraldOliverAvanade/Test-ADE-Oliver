3c1c359e1a4ba77da8aa0b30d691355d23175f41
GeraldOliverAvanade <gerald.oliver.onggi@avanade.com>
2026-02-13 14:16:01 +0900

    Update azuredeploy.tf

--- terraformtest/azuredeploy.tf
+++ terraformtest/azuredeploy.tf
@@ -1,61 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-variable "db_password" {
-  type = string
-}
-
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the resource group to create"
 }
 
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+variable "location" {
+  type        = string
+  description = "Azure region for the resource group (e.g. japaneast)"
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = "asp-terraform"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_app_service" "as" {
-  name                = "as-terraform"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-
-  site_config {
-    linux_fx_version = "DOCKER|nginx"
-  }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_mysql_flexible_server" "mysql" {
-  name                = "mysql-terraform"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  sku_name            = "GP_Standard_D2ds_v4"
-  version             = "5.7"
-  administrator_login = "mysqladmin"
-  administrator_password = var.db_password
-
-  storage {
-    size_gb = 20
-  }
-
-  high_availability {
-    mode = "ZoneRedundant"
-  }
-
-  tags = {
-    environment = "dev"
-  }
+output "resource_group_location" {
+  value = azurerm_resource_group.rg.location
 }
