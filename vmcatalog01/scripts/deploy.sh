f61a3fcf87ba97c6b5165e723039bf2e2ca2f961
GeraldOliverAvanade <gerald.oliver.onggi@avanade.com>
2026-03-03 14:29:00 +0900

    Create deploy.sh

---
+++ vmcatalog01/scripts/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+echo "Signing into Azure using MSI"
+while true; do
+    az login --identity --allow-no-subscriptions --only-show-errors --output none && {
+        echo "Successfully signed into Azure"
+        break
+    } || sleep 5
+done
+
+EnvironmentState="$ADE_STORAGE/environment.tfstate"
+EnvironmentPlan="/environment.tfplan"
+EnvironmentVars="/environment.tfvars.json"
+
+echo "$ADE_OPERATION_PARAMETERS" > $EnvironmentVars
+
+export TF_VAR_resource_group_name=$ADE_RESOURCE_GROUP_NAME
+export ARM_USE_MSI=true
+export ARM_CLIENT_ID=$ADE_CLIENT_ID
+export ARM_TENANT_ID=$ADE_TENANT_ID
+export ARM_SUBSCRIPTION_ID=$ADE_SUBSCRIPTION_ID
+
+echo -e "\n>>>Deploying Terraform...\n"
+terraform init
+terraform plan -no-color -compact-warnings -refresh=true -lock=true -state=$EnvironmentState -out=$EnvironmentPlan -var-file="$EnvironmentVars"
+terraform apply -no-color -compact-warnings -auto-approve -lock=true -state=$EnvironmentState $EnvironmentPlan
+
+tfOutputs=$(terraform output -state=$EnvironmentState -json)
+# Convert Terraform output format to ADE format.
+tfOutputs=$(jq 'walk(if type == "object" then 
+            if .type == "bool" then .type = "boolean" 
+            elif .type == "list" then .type = "array" 
+            elif .type == "map" then .type = "object" 
+            elif .type == "set" then .type = "array" 
+            elif (.type | type) == "array" then 
+                if .type[0] == "tuple" then .type = "array" 
+                elif .type[0] == "object" then .type = "object" 
+                elif .type[0] == "set" then .type = "array" 
+                else . 
+                end 
+            else . 
+            end 
+        else . 
+        end)' <<< "$tfOutputs")
+
+echo "{\"outputs\": $tfOutputs}" > $ADE_OUTPUTS
