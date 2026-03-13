54952d871ac6f1caa7e0c75943f506fd17c5d73e
GeraldOliverAvanade <gerald.oliver.onggi@avanade.com>
2026-03-13 14:54:09 +0900

    Update azuredeploy.tf

--- catalog04latest/azuredeploy.tf
+++ catalog04latest/azuredeploy.tf
@@ -402,7 +402,7 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
 #################
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = var.vm_name
-  computer_name       = local.computer_name
+  computer_name       = "defaultpc"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   size                = var.vm_size
